ec72a552193d8758101e1943f431604bf91f2d57
kemptone <justin.kempton@gmail.com>
2016-08-04 10:24:40 -0700

    adding webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+/* globals __dirname, require */
+
+var path = require("path")
+
+var config = {
+    context : path.join(__dirname, 'src')
+    , entry: [
+        './main.js'
+    ]
+    , output: {
+        path: path.join(__dirname, 'www')
+        , filename: 'bundle.js'
+    }
+    , module : {
+        loaders: [
+            {
+                test: /\.js$/
+                , exclude: /node_modules/
+                , loaders: ['babel']
+            }
+        ]
+    }
+    , resolveLoader: {
+        root: [
+            path.join(__dirname, 'node_modules')
+        ]
+    }
+    , rosolve: {
+        root: [
+            path.join(__dirname, 'node_modules')
+        ]
+    }
+}
+
+module.exports = config
