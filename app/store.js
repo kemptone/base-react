a113f401cdbdca69733fc97daa9056d9db5f2374
kemptone <justin.kempton@gmail.com>
2016-08-10 13:29:40 -0700

    working version with better structure

--- app/store.js
+++ app/store.js
@@ -1,5 +1,10 @@
 import { applyMiddleware, createStore } from "redux"
-import reducer from "./consumer/example/reducers"
+import { combineReducers } from "redux"
+import user from "./consumer/example/user.reducers"
+
+const reducer = combineReducers({
+    user
+})
 
 import thunk from "redux-thunk"
 import promise from "redux-promise-middleware"
