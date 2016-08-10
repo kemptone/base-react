d8bbb84a8d64124b40267cc0f169a9db9191077a
kemptone <justin.kempton@gmail.com>
2016-08-10 13:37:05 -0700

    better clean up

--- app/store.js
+++ app/store.js
@@ -1,14 +1,14 @@
 import { applyMiddleware, createStore } from "redux"
 import { combineReducers } from "redux"
-import user from "./consumer/example/user.reducers"
-
-const reducer = combineReducers({
-    user
-})
+import user from "./consumer/settings/settings.reducers"
 
 import thunk from "redux-thunk"
 import promise from "redux-promise-middleware"
 import logger from "redux-logger"
+
+const reducer = combineReducers({
+    user
+})
 
 const middleware = applyMiddleware(promise(), thunk, logger())
 
